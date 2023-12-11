86299761a6d5591b91c9e50c9d5fd16bc39ea6ff
Alexander Yazdani <110946032+Alexyazdani@users.noreply.github.com>
2023-12-11 00:57:38 -0800

    Update Jitter_RiseFallTime_Automation.py
    .

--- Jitter_RiseFallTime_Automation.py
+++ Jitter_RiseFallTime_Automation.py
@@ -1,7 +1,7 @@
 """
 Alex Yazdani
 05/03/2023
-This file acts as an automation test run for Cisco ODVT jitter, rise and fall time transmitter testing.
+This file acts as an automation test run for jitter, rise and fall time transmitter testing.
 The script connects to a Keysight oscilloscope via GPIB to query the instrument.
 In an actual script for testing, a loop should be used to wait for full scope acquisition.
 
